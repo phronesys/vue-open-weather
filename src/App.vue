78badf697f9072181742ba601600a320cbd19ba9
phronesys <daniel.dazarola@gmail.com>
2021-01-30 14:06:53 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <section class="main-content">
+    <div class="main-content__text">
+      <h2 class="main-content__header">Santiago de Chile</h2>
+      <h4 class="main-content__temp">5°</h4>
+    </div>
+    <div class="main-content__icon-box">
+      <img src="@/assets/logo.png" alt="logo" class="main-content__icon" />
+    </div>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      xml: "",
+    };
+  },
+  mounted() {
+    this.axios.get("http://api.meteored.cl/index.php?api_lang=cl&localidad=18578&affiliate_id=tb26q4vkytw7")
+    .then(response => {
+      console.log(response);
+    }).catch(error => {
+      console.log(error);
+    }) 
+    // const handler = () =>
+    //   fetch("/functions/node-fetch", {
+    //     headers: { accept: "Accept: application/json" },
+    //   })
+    //     .then((x) => x.json())
+    //     .then(({ msg }) => console.log(msg));
+    // console.log(handler);
+  },
+};
 </script>
 
-<style>
+<style lang="scss">
+.main-content {
+  display: inline;
+  @include position-center;
+  width: 60%;
+
+  box-shadow: 1rem 2rem 3rem rgba($color-black, 0.8);
+  border-radius: 5px;
+  text-align: center;
+  &__text {
+    display: inline;
+  }
+  &__icon {
+    width: 3rem;
+  }
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import VueAxios from 'vue-axios';
+import Axios from 'axios';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(VueAxios, Axios)
+
+app.mount("#app");
